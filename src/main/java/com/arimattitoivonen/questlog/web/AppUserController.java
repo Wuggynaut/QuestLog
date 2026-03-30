cf3afb3a553cedcce36ee6f67b2dec90cc70aaad
Wuggy <ari-matti.toivonen@myy.haaga-helia.fi>
2026-03-30 14:37:40 +0300

    Game controller and template progress

--- src/main/java/com/arimattitoivonen/questlog/web/AppUserController.java
+++ src/main/java/com/arimattitoivonen/questlog/web/AppUserController.java
@@ -1,8 +1,16 @@
 package com.arimattitoivonen.questlog.web;
 
+import com.arimattitoivonen.questlog.domain.AppUserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AppUserController {
+
+    private AppUserRepository appUserRepository;
+
+    public AppUserController (AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
 
 }
--- src/main/java/com/arimattitoivonen/questlog/web/GameController.java
+++ src/main/java/com/arimattitoivonen/questlog/web/GameController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.arimattitoivonen.questlog.domain.Game;
@@ -35,5 +36,17 @@ public class GameController {
     public String saveGame(@ModelAttribute Game game) {
         gameRepository.save(game);
         return "redirect:gamelist";
+    }
+
+    @GetMapping("/deletegame/{id}")
+    public String deleteGame(@PathVariable("id") Long id, Model model) {
+        gameRepository.deleteById(id);
+        return "redirect:../gamelist";
+    }
+
+    @GetMapping("/editgame/{id}")
+    public String editGame(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("game", gameRepository.findById(id));
+        return "editgame";
     }
 }
